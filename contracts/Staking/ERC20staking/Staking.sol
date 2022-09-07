c41db74ae4d9c97608d23118c6159337195e716c
Ankit Kathayat <93024110+iamankitkathayat@users.noreply.github.com>
2022-09-07 17:05:35 +0530

    Create Staking.sol

---
+++ contracts/Staking/ERC20staking/Staking.sol
@@ -0,0 +1,156 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+import "../../../node_modules/openzeppelin-solidity/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+import "../../Tokens/Enoch.sol";
+import "hardhat/console.sol";
+
+
+contract Staking is Enoch, Ownable {
+    
+    using SafeMath for uint256;
+    
+    address[] public stakeholders;
+
+    mapping(address => uint256) public stakes;
+
+    mapping(address => uint256) public rewards;
+
+    // constructor(address _owner, uint256 _supply) 
+    //     public
+    // { 
+    //     _mint(_owner, _supply);
+    // }
+
+    // ---------- STAKES ----------
+
+
+    function createStake(uint256 _stake)
+        public
+    {
+        _burn(msg.sender, _stake);
+        if(stakes[msg.sender] == 0) addStakeholder(msg.sender);
+        stakes[msg.sender] = stakes[msg.sender].add(_stake);
+    }
+
+
+    function removeStake(uint256 _stake)
+        public
+    {
+        stakes[msg.sender] = stakes[msg.sender].sub(_stake);
+        if(stakes[msg.sender] == 0) removeStakeholder(msg.sender);
+        _mint(msg.sender, _stake);
+    }
+
+
+    function stakeOf(address _stakeholder)
+        public
+        view
+        returns(uint256)
+    {
+        return stakes[_stakeholder];
+    }
+
+
+    function totalStakes()
+        public
+        view
+        returns(uint256)
+    {
+        uint256 _totalStakes = 0;
+        for (uint256 s = 0; s < stakeholders.length; s += 1){
+            _totalStakes = _totalStakes.add(stakes[stakeholders[s]]);
+        }
+        return _totalStakes;
+    }
+
+    // ---------- STAKEHOLDERS ----------
+
+
+    function isStakeholder(address _address)
+        public
+        view
+        returns(bool, uint256)
+    {
+        for (uint256 s = 0; s < stakeholders.length; s += 1){
+            if (_address == stakeholders[s]) return (true, s);
+        }
+        return (false, 0);
+    }
+
+
+    function addStakeholder(address _stakeholder)
+        public
+    {
+        (bool _isStakeholder, ) = isStakeholder(_stakeholder);
+        if(!_isStakeholder) stakeholders.push(_stakeholder);
+    }
+
+
+    function removeStakeholder(address _stakeholder)
+        public
+    {
+        (bool _isStakeholder, uint256 s) = isStakeholder(_stakeholder);
+        if(_isStakeholder){
+            stakeholders[s] = stakeholders[stakeholders.length - 1];
+            stakeholders.pop();
+        } 
+    }
+
+    // ---------- REWARDS ----------
+    
+
+    function rewardOf(address _stakeholder) 
+        public
+        view
+        returns(uint256)
+    {
+        return rewards[_stakeholder];
+    }
+
+
+    function totalRewards()
+        public
+        view
+        returns(uint256)
+    {
+        uint256 _totalRewards = 0;
+        for (uint256 s = 0; s < stakeholders.length; s += 1){
+            _totalRewards = _totalRewards.add(rewards[stakeholders[s]]);
+        }
+        return _totalRewards;
+    }
+
+
+    function calculateReward(address _stakeholder)
+        public
+        view
+        returns(uint256)
+    {
+        return stakes[_stakeholder] / 100;
+    }
+
+
+    function distributeRewards() 
+        public
+        onlyOwner
+    {
+        for (uint256 s = 0; s < stakeholders.length; s += 1){
+            address stakeholder = stakeholders[s];
+            uint256 reward = calculateReward(stakeholder);
+            rewards[stakeholder] = rewards[stakeholder].add(reward);
+        }
+    }
+
+
+    function withdrawReward() 
+        public
+    {
+        uint256 reward = rewards[msg.sender];
+        rewards[msg.sender] = 0;
+        _mint(msg.sender, reward);
+    }
+}
