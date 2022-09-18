d552ab064c6f40c21ae69322a714c6ed853eee95
Ankit Kathayat <93024110+iamankitkathayat@users.noreply.github.com>
2022-09-18 21:53:05 +0530

    Update StakeSetupV2.test.ts

--- test/ClaimingRewards/StakeSetupV2.test.ts
+++ test/ClaimingRewards/StakeSetupV2.test.ts
@@ -1,1 +1,17 @@
+import { ethers } from "hardhat";
 
+describe("===================>Staking<==================",function () {
+    let StakingInstance: any;
+
+    it('should Stake', async () => {
+        const [owner] = await ethers.getSigners();
+        let ownerAddress = await owner.getAddress();
+        console.log('owner Address: ', ownerAddress);
+
+
+        const AdminRegistry = await ethers.getContractFactory("AdminRegistry");
+        const adminRegistry = await AdminRegistry.deploy(owner.address);
+        await adminRegistry.deployed();
+        console.log("AdminRegistry deployed at ", adminRegistry.address);
+
+        const StakingV2 = await ethers.getContractFactory("StakingV2");
