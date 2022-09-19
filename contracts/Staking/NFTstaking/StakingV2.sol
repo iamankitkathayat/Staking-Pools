dcaebb2d6f74c78f00a2fb9f096ccbfbe40f5108
Ankit Kathayat <93024110+iamankitkathayat@users.noreply.github.com>
2022-09-19 23:42:34 +0530

    Update StakingV2.sol

--- contracts/Staking/NFTstaking/StakingV2.sol
+++ contracts/Staking/NFTstaking/StakingV2.sol
@@ -45,3 +45,93 @@ contract StakingV2 is
 
     event NFTStaked(
         address indexed user,
+        uint256 indexed tokenId,
+        uint256 nftValue,
+        uint256 timestamp
+    );
+
+    // emitted when the user collects all the rewards
+    event RewardsClaimed(address indexed _user, uint256 _stakedTokenId, uint256 _rewardAmount, uint256 _timestamp);
+
+    modifier onlyAdmin() {
+        require(
+            IAdminRegistry(adminRegistry).isAdmin(msg.sender),
+            "AdminRegistry: Restricted to admin."
+        );
+        _;
+    }
+
+    struct StakingDetails {
+        uint256 stakingTimestamp;
+        uint256 NFTvalue;
+        uint256 totalClaimableRewards; 
+        uint256 claimedRewards;
+        uint256 rewardInstallment;
+        uint256 lastWithdrawalTime;
+        uint256 lastRewardAccumulatedTime;
+    }
+
+    function initialize(
+        address _stakingToken,
+        address _rewardToken,
+        uint256 _interestRate,
+        uint256 _stakingPeriod,
+        address _adminRegistry
+    ) external initializer {
+        __UUPSUpgradeable_init();
+        stakingToken = _stakingToken;
+        rewardToken = _rewardToken;
+        APY = _interestRate;
+        STAKING_MONTHS = _stakingPeriod;
+        maxUnclaimableToken = _stakingPeriod - 1;
+        adminRegistry = _adminRegistry;
+    }
+
+    //        ,-.
+    //        `-'
+    //        /|\
+    //         |                    ,----------------.              
+    //        / \                   |    Staking     |              
+    //      Caller                  `-------+--------'              
+    //        |                          stake()                         
+    //        | --------------------------------------------------------->
+    //        |                             |check owner & token approval|                        |
+    //        |                             |----------------------------|
+    //        |                             |     transfers NFT          |
+    //        |                             |                            |
+    //        |                             |                            |
+    //        |                             |     update stake info      |
+    //        |                             | --------------------------->
+    //        |                             |   _calculateRewards()      |
+    //        |                             |NFTvalue*REWARD_CONSTANT|
+    //        |                             | --------------------------->
+    //        |                             |   update reward info       |
+    //        |                             |----.                       |
+    //        |                             |    | emit NFTStaked        |
+    //        |                             |<---'                       |
+    //        |                             |                            |
+    //        |                             |                            |
+    //        | <----------------------------                            |
+    //      Caller                  ,-------+--------.              
+    //        ,-.                   |    Staking     |              
+    //        `-'                   `----------------'              
+    //        /|\
+    //         |
+    //        / \
+
+    function stake(
+        address _user,
+        uint256 _tokenId,
+        uint256 _nftValue
+    ) external whenNotPaused onlyAdmin {
+
+        // check token stake update, if false, then only stake
+        require((IPremiumNFT(stakingToken).getTokenStakedInfo(_tokenId) == false), "Token already staked earlier");
+
+        require(
+            IPremiumNFT(stakingToken).ownerOf(_tokenId) == msg.sender,
+            "Owner does not owns this NFT!"
+        );
+        // Check approval NFT -> this contract
+        require(
+            IPremiumNFT(stakingToken).getApproved(_tokenId) == address(this),
