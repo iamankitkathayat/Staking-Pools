8b45b5ff19fb628f157ec92de07ec5688c5348a2
Ankit Kathayat <93024110+iamankitkathayat@users.noreply.github.com>
2022-09-20 21:01:00 +0530

    Update PremiumNFT.sol

--- contracts/Tokens/PremiumNFT.sol
+++ contracts/Tokens/PremiumNFT.sol
@@ -1,1 +1,55 @@
+// SPDX-License-Identifier: MIT
 
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+
+import "../Registry/IAdminRegistry.sol";
+import "../Staking/Stakable.sol";
+
+// Premium NFTS are collections from KTDs and AZTECH.
+contract PremiumNFT is Stakable, ERC721URIStorage {
+
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
+
+    string public baseURI = "";
+    address public adminRegistry;
+
+    constructor(string memory _name, string memory _symbol, address _adminRegistry) 
+        ERC721(_name, _symbol)
+    {
+        adminRegistry = _adminRegistry;
+    }
+
+    modifier onlyAdmin() {
+        require(
+            IAdminRegistry(adminRegistry).isAdmin(msg.sender),
+            "AdminRegistry: Restricted to admin."
+        );
+        _;
+    }
+
+    function mint(address _owner) public onlyAdmin returns (uint256) {
+        _tokenIds.increment();
+        uint256 newItemId = _tokenIds.current();
+    
+        string memory tokenURI = string(
+            abi.encodePacked(baseURI, Strings.toString(newItemId))
+        );
+        _mint(_owner, newItemId);
+        _setTokenURI(newItemId, tokenURI);
+
+        return newItemId;
+    }
+
+    function burn(uint256 _tokenId) public {
+        _burn(_tokenId);
+    }
+
+    function setBaseURI(string memory _uri) public onlyAdmin {
+        baseURI = _uri;
+    }
+
+}
