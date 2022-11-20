75bd14426bdd946ada6666ee77a857cfb0916a3d
Ankit Kathayat <93024110+iamankitkathayat@users.noreply.github.com>
2022-11-20 21:28:24 +0530

    Update hardhat.config.ts

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -17,8 +17,6 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
   }
 });
 
-// You need to export an object to set up your config
-// Go to https://hardhat.org/config/ to learn more
 
 /**
  * @type import('hardhat/config').HardhatUserConfig
